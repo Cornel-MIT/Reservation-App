9a25c048ffadf3e7a848a6628672b7d05bfe872b
Kronik502 <kronikwenaite@gmail.com>
2025-01-20 09:48:05 +0200

    Working Backend

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,4 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Start the server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
