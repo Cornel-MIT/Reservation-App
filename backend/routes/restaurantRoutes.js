988cfb8d275f0046e15d98680fffce314c2f6f9e
Cornel-MIT <www.ccornel@gmail.com>
2025-01-31 05:09:49 +0200

    Display Restaurants on UserUI

--- backend/routes/restaurantRoutes.js
+++ backend/routes/restaurantRoutes.js
@@ -2,18 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Restaurant = require("../models/restaurantModel");
 
-// Add a new restaurant
-// router.post("/add", async (req, res) => {
-//   try {
-//     const restaurantData = req.body;
-//     const newRestaurant = new Restaurant(restaurantData);
-//     await newRestaurant.save();
-//     res.status(201).json(newRestaurant);
-//   } catch (error) {
-//     console.error("Error saving restaurant:", error);
-//     res.status(400).json({ message: error.message });
-//   }
-// });
 
 router.post("/add", async (req, res) => {
   console.log("Received request body:", req.body); 
@@ -26,6 +14,16 @@ router.post("/add", async (req, res) => {
   } catch (error) {
     console.error("Error saving restaurant:", error);
     res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find(); 
+    res.status(200).json(restaurants); 
+  } catch (error) {
+    console.error("Error fetching restaurants:", error);
+    res.status(500).json({ message: "Error fetching restaurants", error });
   }
 });
 
